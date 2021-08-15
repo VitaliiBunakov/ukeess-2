85f4bef51a4291a224933eb787b2bd1d578b1248
Vitalii.Bunakov <boonakow@gmail.com>
2021-08-15 17:49:58 +0300

    ORMCONFIG works!!!

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,20 +12,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '!Q2w3e4r5t6y',
-      database: 'ukeess_test',
-      entities: [Car, Operation],
-      synchronize: false,
-      migrations: ['src/migration/*.ts'],
-      cli: {
-        migrationsDir: 'migration',
-      },
-    }),
+    TypeOrmModule.forRoot(),
     CarsModule,
     OperationsModule,
     LoadCarsOperationsModule,
